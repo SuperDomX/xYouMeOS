ee0b57551625a8fd12ef6ace680d5faa26ea6823
xopherdeep <heylisten@xtiv.net>
2014-08-26 00:43:44 -0700

    xYouMeOS/ Automated Nightly Push

---
+++ js/infocallout.js
@@ -0,0 +1,110 @@
+
+
+function makeSunEarthDiagram(){
+	var sunEarthDiagram = new THREE.Object3D();
+
+	var distanceToEarth = KMToLY(150000000);
+	var lineGeo = new THREE.Geometry();
+	lineGeo.vertices.push( new THREE.Vector3(0,0,0) );
+	lineGeo.vertices.push( new THREE.Vector3(distanceToEarth,0,0) );
+	var line = new THREE.Line(lineGeo, new THREE.LineBasicMaterial({color:0xffffff}));
+	sunEarthDiagram.add( line );	
+	
+	var earthGyro = new THREE.Gyroscope();
+	earthGyro.position.x = distanceToEarth;	
+	sunEarthDiagram.add( earthGyro );	
+	var earthCallout = makeClamper(0.0000025);	
+	earthGyro.add( earthCallout );
+
+	earthGyro.name = "Sun";
+	attachMarker( earthGyro, 0.001 );
+
+	var sunGyro = new THREE.Gyroscope();
+	sunEarthDiagram.add( sunGyro );
+	var sunCallout = makeClamper(0.000003);
+	sunGyro.add( sunCallout );
+	
+	translating.add(sunEarthDiagram);
+}
+
+var clamperTexture = THREE.ImageUtils.loadTexture( 'images/clamper.png' );
+var clamperMaterial = new THREE.MeshBasicMaterial({
+	map: clamperTexture,
+	depthTest: false,
+	depthWrite: false,
+	transparent: true,
+	side: THREE.DoubleSide,
+	blending: THREE.AdditiveBlending
+});
+
+function makeClamper( scale ){
+	clamperTexture.anisotropy = maxAniso;
+	var plane = new THREE.Mesh( new THREE.PlaneGeometry(1,1), clamperMaterial );
+	plane.scale.setLength( scale );
+	return plane;
+}
+
+function displaySunEarthDiagram(){
+}
+
+function highlightStarHeat(){
+	var seconds = 6.0;
+	toggleHeatVision( true );
+	setTimeout( function(){
+		toggleHeatVision( false );
+	}, seconds * 1000.0 )
+}
+
+function highlightMilkyWay(){
+	var seconds = 6.0;
+	toggleGalacticMeasurement( true );
+	setTimeout( function(){
+		toggleGalacticMeasurement( false );
+	}, seconds * 1000.0 )	
+}
+
+function fadeInRegForm(){
+	fadeInLoginForm(true);
+}
+
+function fadeInLoginForm(reg){
+	 $detailContainer.addClass('about');
+
+      $('#css-container').css('display', 'none');
+
+      $detailContainer.show();
+
+      $.get('login.html', function(data) {
+      	$detailContainer.hide();
+
+        $('#detailBody').html(data);        
+        $detailContainer.fadeIn();
+
+        $('#icon-nav').appendTo('#login');
+
+        $('.box-switcher').parent().on('click touchstart', '.box-switcher', function(e){
+		    e.preventDefault();
+		    var box = $(this).attr('data-switch');
+		    $(this).closest('.box').toggleClass('active');
+		    $('#'+box).closest('.box').addClass('active'); 
+		});
+
+        if(reg){
+
+ 			box =  $('.box-switcher').parent().attr('data-switch');
+		    $('.box-switcher').parent().closest('.box').toggleClass('active');
+		    $('#'+box).closest('.box').addClass('active'); 
+
+        }
+
+      });
+
+      $('#detailTitle').find('span').html('');
+
+      $detailContainer.css({
+        paddingTop: 0
+      });
+
+	  zoomOut(57777);
+      $detailContainer.hide();
+}
---
+++ xYouMeOS.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author heylisten@xtiv.net
+ * @name You Me OS
+ * @desc A 4D Internet Operating System
+ * @version v0.0.1
+ * @icon  
+ * @link  youMeOS
+ * @see community 
+ * @mini ge
+ * @release alpha
+ * @alpha true
+ */
+class xYouMeOS extends Xengine {
+	/**
+		@name index
+		@blox You-In-Verse
+		@desc Center Your Web
+		@icon ge
+	**/
+	function index(){
+		// Also available through $this->Q;		
+		$q = $this->q();
+
+		// Example of Hard-Coded Admin or User Only privledges.
+		$k = $this->Key['is']; 	
+		if( true === $k['admin'] || true === $k['user'] ){
+
+			$table = (true !== $k['admin']) ? 'users' : 'admins';
+
+			$r = array(
+			'data' 	=> $q->Select('portal', $table ,array(
+				'id' => $_SESSION['user']['id']
+			)),		
+			'success' => (!$q->error) ? true : false,
+			'error'	  => (!$q->error) ? null : $this->Q->error
+			); 
+
+			
+
+		} else {		
+			$r = array(
+				// 'data' => null,		
+				'success' => false,
+				'error'	  => 'Not Logged In',
+				'action'  => 'login',
+				//'method'  => 'login' 
+			);
+		}
+		
+		return $r;
+	}
+}
+?>
